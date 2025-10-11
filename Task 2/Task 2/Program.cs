ac0bb534237e9b17a4044005a2f7f08fcfa79b43
Anthony-cabral <20242552@itla.edu.do>
2025-10-11 12:56:30 -0400

    Agregando mi tarea 2

---
+++ Task 2/Task 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"The number entered is {number} and it is Even.");
+            }
+            else
+            {
+                Console.WriteLine($"The number entered is {number} and it is Odd.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
